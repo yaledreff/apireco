f917db9ec05b5fd7c1dc0624d38d1192bd14fb4a
Yann Le Dreff <yaledreff@gmail.com>
2022-08-23 15:52:24 +0200

    test

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def read_root():
 def read_coucou():
     dfArticlesActive = pd.read_csv('/data/dfArticlesActive.csv')
     nbRows = dfArticlesActive.shape[0]
-    return {"message": "Coucou from the API : " + nbRows}
+    return {"message": "Coucou from the API : " + str(nbRows)}
 
 # ****************************** USERS AND ARTICLES DATA MANAGEMENT *****************************
 
